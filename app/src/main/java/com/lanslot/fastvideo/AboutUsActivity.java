cdf15273721fe6e21333aa811a7dd715cbdcd454
jjloveda <513444920@qq.com>
2020-08-07 15:44:59 +0800

    --新增关于我们-免责声明 --修改订阅会员部分

--- app/src/main/java/com/lanslot/fastvideo/AboutUsActivity.java
+++ app/src/main/java/com/lanslot/fastvideo/AboutUsActivity.java
@@ -1,14 +1,18 @@
 package com.lanslot.fastvideo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.lanslot.fastvideo.AOP.Authority.AuthUtils;
 import com.lanslot.fastvideo.Utils.ActionBarUtils;
+import com.lanslot.fastvideo.Utils.DownloadUtils;
 import com.lanslot.fastvideo.Utils.PackageUtils;
 import com.lanslot.fastvideo.Utils.StatusBarUtil;
 import com.zzhoujay.richtext.RichText;
@@ -18,11 +22,14 @@ import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
+import java.util.Date;
+
 @ContentView(R.layout.activity_about_us)
 public class AboutUsActivity extends AppCompatActivity {
 
  @ViewInject(R.id.versionCode)
      TextView versionCode;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +58,18 @@ public class AboutUsActivity extends AppCompatActivity {
     @Event(R.id.privacyPolicy)
     private void onPrivacyPolicyButtonClicked(View v) {
         AuthUtils.getInstance().startActivity(AboutUsActivity.this, PrivacyPolicyActivity.class, null);
+    }
+
+    @Event(R.id.disclaimer)
+    private void ondisclaimerButtonCilcked(View v){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(AboutUsActivity.this).setIcon(R.drawable.mainlogo).setTitle("免责声明")
+                .setMessage(getResources().getString(R.string.disclaimer_content)).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 
     @Override
--- app/src/main/java/com/lanslot/fastvideo/PurchaseActivity.java
+++ app/src/main/java/com/lanslot/fastvideo/PurchaseActivity.java
@@ -1,23 +1,47 @@
 package com.lanslot.fastvideo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.lanslot.fastvideo.AOP.Authority.AuthUtils;
+import com.lanslot.fastvideo.Bean.Setting;
 import com.lanslot.fastvideo.Utils.ActionBarUtils;
 import com.lanslot.fastvideo.Utils.StatusBarUtil;
 import com.zzhoujay.richtext.RichText;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
 
 @ContentView(R.layout.activity_purchase)
 public class PurchaseActivity extends AppCompatActivity {
+    @ViewInject(R.id.monthlyMembership)
+    TextView monMem;
+    @ViewInject(R.id.permanentMember)
+    TextView perMem;
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE };
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +59,8 @@ public class PurchaseActivity extends AppCompatActivity {
         ((TextView) actionBar.findViewById(R.id.title)).setText("订阅会员");
 
         StatusBarUtil.setStatusBarColor(this, R.color.lightcoral);
+        monMem.setText("月度VIP会员："+MyApplication.getSettingByKey(Setting.Key.VIP_USER_MONTH)+"元/月");
+        perMem.setText("永久VIP会员："+MyApplication.getSettingByKey(Setting.Key.VIP_USER_OVER)+"元");
 
     }
 
@@ -45,6 +71,93 @@ public class PurchaseActivity extends AppCompatActivity {
         return false;
     }
 
+    @Event(value = R.id.alipayImg,type = View.OnLongClickListener.class)
+    private boolean onLongClickAlipayImageView(View v){
+        Bitmap  bitmap = Bitmap.createBitmap(v.getWidth(), v.getHeight(),
+                Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(bitmap);
+        v.draw(canvas);
+        FileOutputStream outStream = null;
+        String pngName = new Date().getTime() + ".png";
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/"
+                +Environment.DIRECTORY_DCIM,pngName);
+        System.out.println("------------++:"+file.getPath());
+        if (file.isDirectory()) {// 如果是目录不允许保存
+            return true;
+        }
+        int permission = ActivityCompat.checkSelfPermission(PurchaseActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(PurchaseActivity.this, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
+        try {
+            outStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+            outStream.flush();
+            Toast.makeText(PurchaseActivity.this,file.getPath(),Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(PurchaseActivity.this,"保存失败",Toast.LENGTH_SHORT).show();
+        }finally {
+            try {
+                bitmap.recycle();
+                if(outStream!=null){
+                    outStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                return true;
+            }
+        }
+
+    }
+
+    @Event(value = R.id.wechatImg,type = View.OnLongClickListener.class)
+    private boolean onLongClickWeChatImageView(View v){
+        Bitmap  bitmap = Bitmap.createBitmap(v.getWidth(), v.getHeight(),
+                Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(bitmap);
+        v.draw(canvas);
+        FileOutputStream outStream = null;
+        String pngName = new Date().getTime() + ".png";
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/"
+                +Environment.DIRECTORY_DCIM,pngName);
+        System.out.println("------------++:"+file.getPath());
+        if (file.isDirectory()) {// 如果是目录不允许保存
+            return true;
+        }
+        int permission = ActivityCompat.checkSelfPermission(PurchaseActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(PurchaseActivity.this, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
+        try {
+            outStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+            outStream.flush();
+            Toast.makeText(PurchaseActivity.this,file.getPath(),Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(PurchaseActivity.this,"保存失败",Toast.LENGTH_SHORT).show();
+        }finally {
+            try {
+                bitmap.recycle();
+                if(outStream!=null){
+                    outStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                return true;
+            }
+        }
+
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
