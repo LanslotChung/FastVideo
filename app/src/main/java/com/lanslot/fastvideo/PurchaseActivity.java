da8b3510c645f7ca72d46cd6a444c9495758b53f
jjloveda <513444920@qq.com>
2020-08-12 15:36:46 +0800

    --修改图片保存逻辑

--- app/src/main/java/com/lanslot/fastvideo/PurchaseActivity.java
+++ app/src/main/java/com/lanslot/fastvideo/PurchaseActivity.java
@@ -1,23 +1,30 @@
 package com.lanslot.fastvideo;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.blankj.utilcode.util.ImageUtils;
 import com.lanslot.fastvideo.AOP.Authority.AuthUtils;
 import com.lanslot.fastvideo.Bean.Setting;
 import com.lanslot.fastvideo.Utils.ActionBarUtils;
 import com.lanslot.fastvideo.Utils.StatusBarUtil;
+import com.squareup.picasso.Picasso;
 import com.zzhoujay.richtext.RichText;
 
 import org.xutils.view.annotation.ContentView;
@@ -28,6 +35,9 @@ import org.xutils.x;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Date;
 
 
@@ -37,6 +47,10 @@ public class PurchaseActivity extends AppCompatActivity {
     TextView monMem;
     @ViewInject(R.id.permanentMember)
     TextView perMem;
+    @ViewInject(R.id.wechatImg)
+    ImageView weChatImg;
+    @ViewInject(R.id.alipayImg)
+    ImageView aliPayImg;
 
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
@@ -61,7 +75,8 @@ public class PurchaseActivity extends AppCompatActivity {
         StatusBarUtil.setStatusBarColor(this, R.color.lightcoral);
         monMem.setText("月度VIP会员："+MyApplication.getSettingByKey(Setting.Key.VIP_USER_MONTH)+"元/月");
         perMem.setText("永久VIP会员："+MyApplication.getSettingByKey(Setting.Key.VIP_USER_OVER)+"元");
-
+        Picasso.get().load(MyApplication.getSettingByKey(Setting.Key.USER_RECHANG_ZFB_URL)).into(aliPayImg);
+        Picasso.get().load(MyApplication.getSettingByKey(Setting.Key.USER_RECHANG_WX_URL)).into(weChatImg);
     }
 
     @Override
@@ -74,17 +89,13 @@ public class PurchaseActivity extends AppCompatActivity {
     @Event(value = R.id.alipayImg,type = View.OnLongClickListener.class)
     private boolean onLongClickAlipayImageView(View v){
         Bitmap  bitmap = Bitmap.createBitmap(v.getWidth(), v.getHeight(),
-                Bitmap.Config.RGB_565);
+                Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         v.draw(canvas);
-        FileOutputStream outStream = null;
         String pngName = new Date().getTime() + ".png";
         File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/"
                 +Environment.DIRECTORY_DCIM,pngName);
         System.out.println("------------++:"+file.getPath());
-        if (file.isDirectory()) {// 如果是目录不允许保存
-            return true;
-        }
         int permission = ActivityCompat.checkSelfPermission(PurchaseActivity.this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE);
         if (permission != PackageManager.PERMISSION_GRANTED) {
@@ -92,72 +103,47 @@ public class PurchaseActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(PurchaseActivity.this, PERMISSIONS_STORAGE,
                     REQUEST_EXTERNAL_STORAGE);
         }
-        try {
-            outStream = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
-            outStream.flush();
-            Toast.makeText(PurchaseActivity.this,file.getPath(),Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            e.printStackTrace();
+        boolean ret= ImageUtils.save(bitmap,file.getPath(),Bitmap.CompressFormat.PNG);
+        if(!ret){
             Toast.makeText(PurchaseActivity.this,"保存失败",Toast.LENGTH_SHORT).show();
-        }finally {
-            try {
-                bitmap.recycle();
-                if(outStream!=null){
-                    outStream.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }finally {
-                return true;
-            }
+            return true;
         }
-
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri uri = Uri.fromFile(file);
+        intent.setData(uri);
+        PurchaseActivity.this.sendBroadcast(intent);
+        Toast.makeText(PurchaseActivity.this,file.getPath(),Toast.LENGTH_SHORT).show();
+        return true;
     }
 
     @Event(value = R.id.wechatImg,type = View.OnLongClickListener.class)
     private boolean onLongClickWeChatImageView(View v){
         Bitmap  bitmap = Bitmap.createBitmap(v.getWidth(), v.getHeight(),
-                Bitmap.Config.RGB_565);
+                Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         v.draw(canvas);
-        FileOutputStream outStream = null;
         String pngName = new Date().getTime() + ".png";
         File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/"
                 +Environment.DIRECTORY_DCIM,pngName);
         System.out.println("------------++:"+file.getPath());
-        if (file.isDirectory()) {// 如果是目录不允许保存
-            return true;
-        }
         int permission = ActivityCompat.checkSelfPermission(PurchaseActivity.this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE);
         if (permission != PackageManager.PERMISSION_GRANTED) {
             // We don't have permission so prompt the user
             ActivityCompat.requestPermissions(PurchaseActivity.this, PERMISSIONS_STORAGE,
                     REQUEST_EXTERNAL_STORAGE);
+        }
+        boolean ret= ImageUtils.save(bitmap,file.getPath(),Bitmap.CompressFormat.PNG);
+        if(!ret){
+            Toast.makeText(PurchaseActivity.this,"保存失败",Toast.LENGTH_SHORT).show();
             return true;
         }
-        try {
-            outStream = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
-            outStream.flush();
-            Toast.makeText(PurchaseActivity.this,file.getPath(),Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(PurchaseActivity.this,"保存失败",Toast.LENGTH_SHORT).show();
-        }finally {
-            try {
-                bitmap.recycle();
-                if(outStream!=null){
-                    outStream.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }finally {
-                return true;
-            }
-        }
-
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri uri = Uri.fromFile(file);
+        intent.setData(uri);
+        PurchaseActivity.this.sendBroadcast(intent);
+        Toast.makeText(PurchaseActivity.this,file.getPath(),Toast.LENGTH_SHORT).show();
+        return true;
     }
     @Override
     protected void onDestroy() {
